6d7147d1a06c0fc0a49109d3a50a160f5223680d
cptdanhook <dfehook@gmail.com>
2024-12-03 15:41:58 +0000

    Delete SQL tut1.sql

--- SQL tut1.sql
+++
@@ -1,8 +0,0 @@
-SELECT * 
-FROM employee_demographics;
-
-SELECT first_name, age, birth_date, age + 10 AS older_age
-FROM employee_demographics;
-
-SELECT DISTINCT first_name, gender
-FROM employee_demographics;
